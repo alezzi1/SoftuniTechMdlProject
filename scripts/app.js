231e4026284d9ad0558179ae134a864e7b800721
alezzi1 <a.kasteliev@gmail.com>
2016-08-27 12:40:01 +0300

    v4

---
+++ scripts/app.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Alezzi on 26.8.2016 г..
+ */
+
+let baseUrl = "https://baas.kinvey.com";
+let appKey = "kid_HyI6vATc";
+let appSecret = "5e35540b0af04582b1c1e32136737d5b";
+var _guestCredentials = "";

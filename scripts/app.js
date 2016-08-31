b178c2458c4d1b8973427fa1c2eddeff902209d3
alezzi1 <a.kasteliev@gmail.com>
2016-09-01 00:02:56 +0300

    V.7 Final
    Маратон по прехвърляне на стара идея на ново тяло ... щета, искам да
    спя!

--- scripts/app.js
+++ scripts/app.js
@@ -1,260 +1,197 @@
+const kinveyBaseUrl = "https://baas.kinvey.com/";
+const kinveyAppKey = "kid_HyI6vATc";
+const kinveyAppSecret = "5e35540b0af04582b1c1e32136737d5b";
 
-    const kinveyServiceBaseUrl = "https://baas.kinvey.com";
-    const kinveyAppID = "kid_HyI6vATc";
-    const kinveyAppSecret = "5e35540b0af04582b1c1e32136737d5b";
-    const appUrl = kinveyServiceBaseUrl + "user/" + kinveyAppID;
-    let _guestCredentials = "9a4f7319-7a0b-4f05-848a-e0ef7c237bb5.Qh/NzEG4MKzIqJ5+1dRPsL3s5EWpzRAB7gYPkY+W5yY=";
-
-    function showView(viewID) {
-        $('main > section').hide();
-
-        $('#' + viewID).show();
-    }
-
-    $(function () {
+function showView(viewName) {
+    $('main > section').hide();
+    $('#' + viewName).show();
+}
+function showHideMenuLinks() {
+    $("linkHome").show();
+    if (sessionStorage.getItem('authToken') == null) {
         
-        $('#home').click("home");
-        $('#rates').click('rates');
-        $('#newPost').click('newPost');
-        $('#posts').click('posts');
-        $('#login').click('login');
-        $('#reg').click('reg');
+        $("#linkPost").hide();
+        $("#linkNewPost").hide();
+        $("#linkLogIn").show();
+        $("#linkSingIn").show();
+        $("#linkLogOut").hide();
+    } else {
+        
+        $("#linkPost").show();
+        $("#linkNewPost").show();
+        $("#linkLogIn").hide();
+        $("#linkSingIn").hide();
+        $("#linkLogOut").show();
+    }
+}
+function showInfo(message) {
+    $('#infoBox').text(message);
+    $('#infoBox').show();
+    setTimeout(function () { $('infoBox').fadeOut() }, 2500);
+}
+function showError(errorMsg) {
+    $('#errorBox').text("Error: " + errorMsg);
+    $('#errorBox').show();
+    setTimeout(function () { $('errorBox').fadeOut() }, 2500);
+}
+$(function () {
+    showHideMenuLinks();
+    showView('viewHome');
+    $("#linkHome").click(showHomeView);
+    $("#linkPost").click(showPostView);
+    $("#linkNewPost").click(showNewPostView);
+    $("#linkLogIn").click(showLogInView);
+    $("#linkSingIn").click(showSingInView);
+    $("#linkLogOut").click(logout);
 
-        showHideNavigationLinks();
-        showView();
-
-        $('#formLogin').submit(function (e) {
-            e.preventDefault();
-            login()
-        });
-        $('#formRegister').submit(function (e) {
-            e.preventDefault();
-            register();
-        });
-        $('#form-createBook').submit(function (e) {
-            e.preventDefault();
-            createBook();
-        });
-        $('body').on("submit", ".formCreateComment", function (e) {
-            e.preventDefault();
-            let context = $(e.target);
-            let bookData = context.find(".bookData").val();
-            let commentText = context.find('.commentText').val();
-
-            let commentAuthor = context.find(".commentAuthor").val();
-            addBookComment(JSON.parse(bookData), commentText, commentAuthor);
-        });
+    $("#formLogin").submit(function(e) { e.preventDefault(); login(); });
+    $("#formRegister").submit(function(e) { e.preventDefault(); register(); });
+    $("#formCreatePost").submit(function(e) { e.preventDefault(); createPost(); });
+    $(document).on({
+        ajaxStart: function() { $("#loadingBox").show()},
+        ajaxStop: function() { $("#loadingBox").hide()}
     });
-
-    function showHideNavigationLinks() {
-        let loggedIn = sessionStorage.getItem('authToken') != null;
-        if (loggedIn) {
-            
-            $('#home').show();
-            $('#rates').show();
-            $('#newPost').show();
-            $('#posts').show();
-            $('#login').hide();
-            $('#reg').hide();
-
-
+});
+function showHomeView() {
+    showView('viewHome');
+}
+function showLogInView() {
+    showView('viewLogIn');
+}
+function login() {
+    const kinveyLoginUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
+    const kinveyAuthHeaders = {
+        'Authorization': "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret)
+    };
+    let userData = {
+        username: $('#loginUser').val(),
+        password: $('#loginPass').val()
+    };
+    $.ajax({
+        method: "POST",
+        url: kinveyLoginUrl,
+        headers: kinveyAuthHeaders,
+        Data: userData,
+        success: loginSuccess,
+        error: handleAjaxError
+    });
+    function loginSuccess(response) {
+        let userAuth = response._kmd.authtoken;
+        sessionStorage.setItem('authToken',userAuth);
+        showHideMenuLinks();
+        showPostView();
+        showInfo('Login sucessful.');
+    }
+}
+function handleAjaxError(response) {
+    let errorMsg = JSON.stringify(response);
+    if (response.readyState === 0)
+        errorMsg = "Cannot connect due network error.";
+    if (response.responseJSON && response.responseJSON.description)
+        errorMsg = response.responseJSON.description;
+    showError(errorMsg);
+}
+function showSingInView() {
+    showView('viewSingIn');
+}
+function register() {
+    const kinveyRegisterUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
+    const kinveyAuthHeaders = {
+        'Authorization': "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret)
+    };
+    let userData = {
+        username: $('#registerUser').val(),
+        password: $('#registerPass').val()
+    };
+    $.ajax({
+        method: "POST",
+        url: kinveyRegisterUrl,
+        headers: kinveyAuthHeaders,
+        data: userData,
+        success: registerSuccess,
+        error: handleAjaxError
+    });
+    function registerSuccess(response) {
+        let userAuth = response._kmd.authtoken;
+        sessionStorage.setItem('authToken', userAuth);
+        showHideMenuLinks();
+        showPostView();
+        showInfo('User registration successful.');
+    }
+}
+function showPostView() {
+    $('#Posts').empty();
+    showView('viewPost');
+    const kinveyBooksUrl = kinveyBaseUrl + "appdata/" + kinveyAppKey + "/Posts";
+    const kinveyAuthHeaders = {
+        'Authorization': "Kinvey " + sessionStorage.getItem('authToken')
+    };
+    $.ajax({
+        method: "GET",
+        url: kinveyBooksUrl,
+        headers: kinveyAuthHeaders,
+        success: loadGamesSuccess,
+        error: handleAjaxError
+    });
+}
+function loadGamesSuccess(Posts) {
+    showInfo('Posts loaded.');
+    if (Posts.length == 0) {
+        $('#Posts').text('No new posts on the site.');
+    } else {
+        let PostsTable = $('<table>')
+            .append($('<tr>').append(
+                '<th>Game name</th>',
+                '<th>Genre</th>',
+                '<th>Release Date</th>',
+                '<th>Developer</th>',
+                '<th>Our Raiting</th>',
+                '<th>Post</th>')
+            );
+        for (let Post of Posts) {
+            PostsTable.append($('<tr>').append(
+                $('<td>').text(Post.name),
+                $('<td>').text(Post.genre),
+                $('<td>').text(Post.release),
+                $('<td>').text(Post.developer),
+                $('<td>').text(Post.raiting),
+                $('<td>').text(Post.post))
+            );
         }
-        else {
-            $('#home').show();
-            $('#rates').show();
-            $('#newPost').hide();
-            $('#posts').show();
-            $('#login').show();
-            $('#reg').show();
-
-        }
+        $('#Posts').append(PostsTable);
     }
-
-    function login() {
-        let authBase64 = btoa(kinveyAppID + ':' + kinveyAppSecret);
-        let appUrl = kinveyServiceBaseUrl + "user/" + kinveyAppID + '/login';
-        $.ajax({
-            method: "POST",
-            url: appUrl,
-            headers: {"Authorization": "Basic " + authBase64},
-            data: {
-                username: $('#loginUser').val(),
-                password: $('#loginPass').val()
-            },
-            success: loginSuccess,
-            error: handleAjaxError
-        });
-        function loginSuccess(data, status) {
-            let userAuth = data._kmd.authtoken;
-            sessionStorage.setItem('authToken', userAuth);
-            listBooks();
-            showHideNavigationLinks();
-            showInfo('Login successful')
-        }
+}
+function showNewPostView() {
+    showView('viewNewPost');
+}
+function createPost() {
+    const kinveyBooksUrl = kinveyBaseUrl + "appdata/" + kinveyAppKey + "/Posts";
+    const kinveyAuthHeaders ={
+        'Authorization': "Kinvey " + sessionStorage.getItem('authToken')
+    };
+    let bookData = {
+        name: $('#gameName').val(),
+        genre: $('#gameGenre').val(),
+        release: $('#gameDate').val(),
+        developer: $('#developer').val(),
+        raiting: $('#raiting').val(),
+        post: $('#post').val()
+    };
+    $.ajax({
+        method: "POST",
+        url: kinveyBooksUrl,
+        headers: kinveyAuthHeaders,
+        data: bookData,
+        success: createPostsSuccess(),
+        error: handleAjaxError
+    });
+    function createPostsSuccess(response) {
+        showPostView();
+        showInfo('Game review added');
     }
-
-    function register() {
-        let authBase64 = btoa(kinveyAppID + ':' + kinveyAppSecret);
-        let appUrl = kinveyServiceBaseUrl + "user/" + kinveyAppID + '/';
-        $.ajax({
-            method: "POST",
-            url: appUrl,
-            data: {
-                username: $('#registerUser').val(),
-                password: $('#registerPass').val()
-            },
-            headers: {"Authorization": "Basic " + authBase64},
-            success: function (response) {
-                let userAuth = response._kmd.authtoken;
-                sessionStorage.setItem('authToken', userAuth);
-                showHideNavigationLinks();
-                showInfo('User registration successful')
-            },
-            error: handleAjaxError
-        });
-    }
-
-    function createBook() {
-        const kinveyBooksURL = kinveyServiceBaseUrl + 'appdata/' + kinveyAppID + '/books';
-        const kinveyAuthHeaders = {"Authorization": "Kinvey " + sessionStorage.getItem('authToken')};
-        let bookData = {
-            title: $('#bookTitle').val(),
-            author: $('#bookAuthor').val(),
-            description: $('#bookAuthor').val(),
-
-        };
-        $.ajax({
-            method: 'POST',
-            url: kinveyBooksURL,
-            headers: kinveyAuthHeaders,
-            data: bookData,
-            success: createBooksSuccess,
-            error: handleAjaxError
-        })
-        function createBooksSuccess(books) {
-            listBooks();
-            showInfo('Book created.')
-
-        }
-    }
-
-    function logout() {
-        $.ajax({
-            method: "POST",
-            url: appUrl + '_logout',
-            headers: {"Authorization": "Kinvey " + sessionStorage.clear()},
-        });
-        showHideNavigationLinks();
-        showView('homeView')
-    }
-
-    function handleAjaxError(response) {
-        let errorMessage = JSON.stringify(response);
-        if (response.readyState == 0) {
-            errorMessage = 'Cannot connect due to network error.';
-        }
-        if (response.responseJSON && response.responseJSON.description) {
-            errorMessage = response.responseJSON.description;
-        }
-        showError(errorMessage)
-    }
-
-    function showError(errorMsg) {
-        $('#errorBox').text('Error: ' + errorMsg);
-        $('#errorBox').show();
-    }
-
-    function listBooks() {
-        $('#books').empty();
-        showView('viewListBook');
-
-        const kinveyBooksURL = kinveyServiceBaseUrl + 'appdata/' + kinveyAppID + '/books';
-        const kinveyAuthHeaders = {"Authorization": "Kinvey " + sessionStorage.getItem('authToken')};
-        $.ajax({
-            method: 'GET',
-            url: kinveyBooksURL,
-            headers: kinveyAuthHeaders,
-            success: loadBooksSuccess,
-            error: handleAjaxError
-        })
-        function loadBooksSuccess(books) {
-            showInfo('Books loaded.')
-            if (books.lenght == 0) $('#books').text('No books in the library')
-
-            else {
-
-                let booksTable = $('<table>')
-                    .append($('<tr>').append(
-                        '<th>Title</th>',
-                        '<th>Author</th>',
-                        '<th>Description</th>')
-                    );
-                for (let book of books) {
-                    let current = book;
-                    let row = $('<tr>').append($('<td></td>').text(book.title)).append($('<td></td>').text(book.author)).append($('<td></td>').text(book.description));
-
-                    row.click(function () {
-                        let row = $(this).next();
-                        if (row.css('display') == 'none') {
-                            row.fadeIn();
-                            row.css('display', 'table-cell').css('background', '#85001f')
-                            row.find('form div').css('display', 'inline-block');
-                        } else {
-                            row.fadeOut();
-                            row.find($('input[type=text]')).val();
-                        }
-                    })
-
-                    // generates a form for each book(row) with 3 divs each
-                    // with 2 text inputs and 1 submit input
-
-                    booksTable.append(row).append($('<td colspan="3" class="commentRow">').append(function () {
-                        for (var ke in book.comments) {
-                            if (book.comments.hasOwnProperty(ke)) {
-                                $(this).append($("<div></div>").text(book.comments[ke].textComment), $("<div>").text(" --" + book.comments[ke].author));
-                            }
-                        }
-                    }).append($('<form class="formCreateComment" style="display: inline-block">').append($('<div></div>').text("Comment: ").append($('<input type="text" class="commentText" required="true">'))
-                    ).append($('<div></div>').text('Author: ').append($('<input type="text" class="commentAuthor" required="true">'))
-                        .append($('<input type="hidden" class="bookData">').attr("value", JSON.stringify(book))
-                        ).append($('<div></div>').append($('<input type="submit" class="commentSubmit" value="Add Comment">'))
-                        )
-                    )));
-
-                }
-                $('#books').append(booksTable)
-
-
-            }
-        }
-    }
-
-    function addBookComment(bookData, commentText, commentAuthor) {
-        const kinveyBooksUrl = kinveyServiceBaseUrl + "appdata/" + kinveyAppID + "/books";
-        const kinveyHeaders = {
-            'Authorization': "Kinvey " + sessionStorage.getItem('authToken'),
-            'Content-type': 'application/json'
-        };
-        if (!bookData.comments) {
-            bookData.comments = [];
-        }
-        bookData.comments.push({textComment: commentText, author: commentAuthor});
-        let stringy = JSON.stringify(bookData);
-
-        $.ajax({
-            method: "PUT",
-            url: kinveyBooksUrl + '/' + bookData._id,
-            headers: kinveyHeaders,
-            data: stringy,
-            success: addBookCommentSuccess,
-            error: handleAjaxError
-        });
-        function addBookCommentSuccess(response) {
-            listBooks();
-            showInfo('Book comment added.')
-        }
-    }
-
-    function showCommentBox() {
-        $('#add-comment').show()
-    }
+}
+function logout() {
+    sessionStorage.clear();
+    showHideMenuLinks();
+    showView();
+}
